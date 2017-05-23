771d25719c892b6753d819f41db620e624d61667
owner <owner@owner-PC>
2017-05-23 11:49:02 +0900

    branch에서 수정

--- test_git/src/kr/co/sist/test/test.java
+++ test_git/src/kr/co/sist/test/test.java
@@ -5,6 +5,10 @@ public class test {
 	public static void main(String[] args) {
 		int i = 0;
 		System.out.println("변수값 i="+i);
+		// hahaha2가 수정했습니다
+		int j=0;
+		System.out.println("j+"+j);
+		
 	}// main
 
 }// class
